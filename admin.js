ee55c6850b7f789de096606267744c1dd1d3e0a7
Rouf Fadilah <rouf.learn@gmail.com>
2026-04-19 21:05:14 +0700

    Update Firebase imports and initialization

--- admin.js
+++ admin.js
@@ -17,10 +17,14 @@ onAuthStateChanged(auth, async (user) => {
     }
 });
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// GANTI DENGAN CONFIG FIREBASE ANDA (Sama seperti di app.js sebelumnya)
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "API_KEY_ANDA",
   authDomain: "cbt-cikalharapan.firebaseapp.com",
