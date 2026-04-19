b51c553db6d6544871008c5060877d059ce20d63
Rouf Fadilah <rouf.learn@gmail.com>
2026-04-19 21:09:01 +0700

    Update Firebase SDK version and configuration

--- login.js
+++ login.js
@@ -2,9 +2,27 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebas
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
-const firebaseConfig = {
-  // Masukkan Config Anda di sini
-};
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBahvtobUXJUkIYmqyFOKxsdAcEt7WbK0E",
+    authDomain: "cbt-smaich.firebaseapp.com",
+    projectId: "cbt-smaich",
+    storageBucket: "cbt-smaich.firebasestorage.app",
+    messagingSenderId: "549681496019",
+    appId: "1:549681496019:web:9e557b569413cdca3ff926",
+    measurementId: "G-MPX858Y2QS"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
